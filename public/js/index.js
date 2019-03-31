0c54b990a02e278273da3ea3231f91e760aa430f
Amir PC <amirhasankntu@gmail.com>
2019-03-31 16:05:57 -0400

    retrieving Categories from database to front end

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
   console.log('hello there!');
 
-    var useRetrievedData = function (data){
-      $("#retrievedData").html("<p>Product is " + data[0].productName +"</p>")
-    }
+  var useRetrievedData = function (data) {
+    $("#retrievedData").html("<p>Product is " + data[0].productName + "</p>")
+  }
 
-  $(".btn_search").on("click", function(e) {
+  $(".btn_search").on("click", function (e) {
 
     e.preventDefault();
     var searchBar = $("#search-bar").val();
@@ -13,15 +13,30 @@ $(document).ready(function () {
 
     $.get({
       url: `api/search?term=${searchBar}`,
-      success: function(data){
-      console.log(data);
-      window.location.href = "/product?search_bar=" + searchBar;
+      success: function (data) {
+        console.log(data);
+        window.location.href = "/product?search_bar=" + searchBar;
       }
-    }).then(function(){
+    }).then(function () {
       //window.location.href = "http://localhost:5000/product";
       console.log("you're redirected to the product page");
       console.log(data);
       useRetrievedData();
     })
   });
+
+  var retrieveCategories = function () {
+    $.get({
+      url: `api/categories`,
+      success: function (data) {
+        var i;
+        for (i = 0; i < data.length; i++) {
+          console.log("categories are :  " + data[i].categoriesName)
+        }
+
+      }
+    })
+  }
+
+  retrieveCategories();
 });
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -15,7 +15,7 @@ module.exports = function (app) {
         record.get('Item') === term ? results.push({
           'productName': record.get('Name'),
           'productLink': record.get('Link'),
-          'productCategory': record.get('Category')
+          'productCategory': record.get('CategoryText')
         }) : null
 
       });
@@ -31,6 +31,32 @@ module.exports = function (app) {
 
     })
   });
+
+  app.get("/api/categories", function (req, res) {
+
+    let categories = [];
+    base('Categories').select({
+      view: "Grid view"
+    }).eachPage(function page(records, fetchNextPage) {
+      // This function (`page`) will get called for each page of records.
+
+      records.forEach(function (record) {
+        console.log('Retrieved', record.get('Name'));
+        categories.push({
+          'categoriesName' : record.get('Name')
+        })
+      });
+
+      // To fetch the next page of records, call `fetchNextPage`.
+      // If there are more records, `page` will get called again.
+      // If there are no more records, `done` will get called.
+      fetchNextPage();
+
+    }, function done(err) {
+      if (err) { console.error(err); return; }
+      res.json(categories);
+    });
+  })
 
 
 };
