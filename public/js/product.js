0b527f30ca4f4451adf156ade08acdf353a546c9
Amir PC <amirhasankntu@gmail.com>
2019-03-31 15:02:27 -0400

    updated product page styling, still need work for categories

--- public/js/product.js
+++ public/js/product.js
@@ -1,32 +1,41 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    var url = window.location.search;
-    var searchBar;
-    var searchBarPretty;
-    var item = $("#item")
-    var itemSub = $("#itemSub");
-    var amazonBtn = $("#amazonBtn");
+  var url = window.location.search;
+  var searchBar;
+  var searchBarPretty;
+  var item = $("#item")
+  var itemSub = $("#itemSub");
+  var amazonBtn = $("#amazonBtn");
 
-    if (url.indexOf("?search_bar=") !== -1) {
-        searchBar = url.split("=")[1];
-        searchBar = searchBar.replace(/[^a-zA-Z0-9 ]/g, " ")
-        searchBarPretty = searchBar.replace(/[^a-zA-Z ]/g, "")
-        getProductData(searchBarPretty);
+  if (url.indexOf("?search_bar=") !== -1) {
+    searchBar = url.split("=")[1];
+    searchBar = searchBar.replace(/[^a-zA-Z0-9 ]/g, " ")
+    searchBarPretty = searchBar.replace(/[^a-zA-Z ]/g, "")
+    getProductData(searchBarPretty);
 
-      }
+  }
 
-      // Gets post data for a post if we're editing
+  // Gets post data for a post if we're editing
   function getProductData(product) {
     $.get({
-        url: `api/search?term=${product}`,
-        success: function(data){
+      url: `api/search?term=${product}`,
+      success: function (data) {
         item.html(data[0].productName);
-        itemSub.html("in " + data[0].productCategory[0] +" (category, needs fixed)");
+        itemSub.html("in " + data[0].productCategory[0] + " (!!category, needs fixed!!)");
         amazonBtn.attr("href", data[0].productLink);
         console.log(data);
-        console.log(data[0].productLink)
-        }
+        //console.log(data[0].productLink)
+        //getCategoryName(data[0].productCategory[0])
+      }
     });
   }
 
+/*   function getCategoryName(categoryId) {
+    $.get({
+      url: `api/category?id=${categoryId}`,
+      success: function (data1) {
+        console.log("this is the new category  " + data1)
+      }
+    })
+  } */
 })
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,37 +1,36 @@
 const Airtable = require('airtable');
 
-let base = new Airtable({apiKey: 'keyXdjNsLcCtpNcVT'}).base('app11pd9foOq6otfl');
+let base = new Airtable({ apiKey: 'keyXdjNsLcCtpNcVT' }).base('app11pd9foOq6otfl');
 
 module.exports = function (app) {
 
-  app.get("/api/search", function(req, res){
+  app.get("/api/search", function (req, res) {
 
     const term = req.query.term;
     let results = []
     base('Products').select({
-      view:  'Grid view'
-    }).eachPage(function(records, fetchNextPage) {
-      records.forEach(function(record) {
+      view: 'Grid view'
+    }).eachPage(function (records, fetchNextPage) {
+      records.forEach(function (record) {
         record.get('Item') === term ? results.push({
           'productName': record.get('Name'),
-          'productLink' : record.get('Link'),
-          'productCategory' : record.get('Category')
+          'productLink': record.get('Link'),
+          'productCategory': record.get('Category')
         }) : null
-    });
 
-    // To fetch the next page of records, call `fetchNextPage`.
-    // If there are more records, `page` will get called again.
-    // If there are no more records, `done` will get called.
-    fetchNextPage();
+      });
+
+      // To fetch the next page of records, call `fetchNextPage`.
+      // If there are more records, `page` will get called again.
+      // If there are no more records, `done` will get called.
+      fetchNextPage();
 
     }, function done(err) {
-      if (err) {console.log(err); return;}
+      if (err) { console.log(err); return; }
       res.json(results);
 
     })
-  })
-
-
+  });
 
 
 };
